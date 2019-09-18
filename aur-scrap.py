3ccc5361d72e6542ddebcf2407dfc6974767fdb7
Rizwan Hasan <rizwan.hasan486@gmail.com>
2019-09-19 03:16:08 +0600

    update

--- aur-scrap.py
+++ aur-scrap.py
@@ -1,169 +1,130 @@
-#!/usr/bin/env python
-# coding: utf-8
+#!/usr/bin/env python3
+
+import json
 import os
-import sqlite3
-import subprocess
 import sys
-from glob import glob
-from random import choice as randomChoice
-from time import sleep
-from urllib import request
-
-from bs4 import BeautifulSoup
-
-Packages_Location_With_Name: str = 'x86_64/*.pkg.tar.xz'
-Database_Location_With_Name: str = 'AurScrapDB.sqlite3'
+import urllib
+import urllib.error
+import urllib.request
+from os import listdir
+from os.path import isfile, join
+from pprint import pprint
 
 
-class AurScrapper:
+def open_main_json_file():
+    # Open the main json file and save it as dictionary.
+    with open("AurScrapDB.json", 'r', encoding='UTF-8') as main_file:
+        datafile: dict = json.loads(main_file.read())  # Use as main dictionary through out the whole code.
 
-    def __init__(self):
-        # Variables ↓
-        self.__localPkgLoc: str = Packages_Location_With_Name
-        self.__rand: tuple = (0.01, 0.01)
-        # Database Operations ↓
-        self.__dbLoc: str = Database_Location_With_Name
-        self.__conn = sqlite3.connect(self.__dbLoc)
+    return datafile
 
-    def __scrap(self, pkgName: str):
-        sleep(randomChoice(self.__rand))
-        url: str = 'https://aur.archlinux.org/packages/{0}/'.format(pkgName)
-        page = request.urlopen(url)
-        soup = BeautifulSoup(page, 'html.parser')
-        nameVer = soup.find('div', attrs={'id': 'pkgdetails'})
-        nameVer = nameVer.find('h2')
-        nameVer = nameVer.text
-        # git: str = soup.find('table', attrs={'id': 'pkginfo'}).find('a')
-        return {
-            "name": pkgName,
-            "version": nameVer.split(' ').pop(),
-            "url": url,
-            "git": 'https://aur.archlinux.org/{0}.git'.format(pkgName)
-        }
 
-    def aurDbUpdate(self):
-        cusror_One = self.__conn.cursor()
-        cusror_Two = self.__conn.cursor()
-        cusror_One.execute("SELECT aur_pkg_name FROM AUR")
-        row = cusror_One.fetchone()
-        while row:
-            pkgName = row[0]
-            data = self.__scrap(pkgName)
-            print(data)
-            sql: str = """
-                UPDATE AUR
-                SET aur_pkg_version = ?,
-                aur_pkg_url = ?,
-                aur_pkg_git = ?
-                WHERE aur_pkg_name = ?
-            """
-            sqlVar: tuple = (data.get('version'), data.get('url'), data.get('git'), data.get('name'))
-            cusror_Two.execute(sql, sqlVar)
-            row = cusror_One.fetchone()
-        self.__conn.commit()
+def update_main_json_file(datafile):
+    with open("AurScrapDB.json", 'w', encoding='UTF-8') as updated_file:
+        json.dump(datafile, updated_file, indent=2, sort_keys=True)
 
-    def localDbUpdate(self):
-        cursor = self.__conn.cursor()
-        # noinspection SqlWithoutWhere
-        cursor.execute("DELETE FROM Repository")
-        for i in glob(self.__localPkgLoc):
-            tmp = os.path.basename(i)
-            tmp = tmp.replace('-any.pkg.tar.xz', '')
-            tmp = tmp.replace('-x86_64.pkg.tar.xz', '')
-            tmp = tmp.split('-')
-            pkgName: str = '-'.join(tmp[:-2])
-            pkgVersion: str = '-'.join(tmp[-2:])
-            sql: str = "INSERT INTO Repository (local_pkg_name, local_pkg_version) VALUES (?, ?)"
-            sqlVar: tuple = (pkgName, pkgVersion)
-            print(sqlVar)
-            cursor.execute(sql, sqlVar)
-        self.__conn.commit()
 
-    def check(self):
-        cursor = self.__conn.cursor()
-        sql: str = """
-            SELECT AUR.local_pkg_name,
-                AUR.aur_pkg_version,
-                AUR.aur_pkg_name,
-                Repository.local_pkg_version,
-                AUR.aur_pkg_url,
-                AUR.aur_pkg_git
-            FROM AUR
-            INNER JOIN Repository ON Repository.local_pkg_name = AUR.local_pkg_name AND
-                                     Repository.local_pkg_version != AUR.aur_pkg_version;
-        """
-        cursor.execute(sql)
-        data = cursor.fetchall()
-        for i in range(len(data)):
-            print(
-                "{0}.\n"
-                "\tPackage name: {1}\n"
-                "\tPackage version: {2}\n"
-                "\tAUR name: {3}\n"
-                "\tAUR version: {4}\n"
-                "\tAUR link: {5}\n"
-                "\tAUR Git Link: {6}".format(i + 1,
-                                             data[i][0],
-                                             data[i][3],
-                                             data[i][2],
-                                             data[i][1],
-                                             data[i][4],
-                                             data[i][5],
-                                             )
-            )
-        print("\nTotal %d package need to be updated.\n" % len(data))
+def set_data(f_str, datafile, filenames_list):
+    for f_name in filenames_list:
+        if f_str in f_name:
+            existing_version = f_name.replace((f_str + "-"), "")
+            datafile[f_str]['version'] = str(existing_version)  # Set existing version, as in directory.
+            url: str = "https://aur.archlinux.org/packages/"
+            datafile[f_str]['aur_link'] = url + str(datafile[f_str]['aur_name']) + "/"  # Set aur_link.
 
-    def close(self):
-        self.__conn.close()
+
+def parse_json(aur_name):
+    try:
+        url: str = 'https://aur.archlinux.org/rpc/?v=5&type=info&arg[]={0}'.format(aur_name)
+        response: str = urllib.request.urlopen(url).read().decode('UTF-8')  # Get response from URL
+
+    except urllib.error.HTTPError:
+        print("HTTP Error!")
+
+    else:
+        return json.loads(response)['results'][0]['Version']
+
+
+def update_local(datafile):
+    # Find the name of files in the package directory.
+    directory_name = "x86_64"
+    filenames_list: list = [((filename.replace(".pkg.tar.xz", "")).replace("-any", "")).replace("-x86_64", "")
+                            for filename in listdir(directory_name)
+                            if isfile(join(directory_name, filename))]
+    # Set the main info like version and aur_link in the datafile(main dictionary file).
+    for key in datafile.keys():
+        set_data(key, datafile, filenames_list)
+    return datafile
+
+
+def update_from_api(datafile):
+    # Parse 'aur_version' from API.
+    it: int = 1
+    total: int = len(datafile.keys())
+    for key in datafile.keys():
+        sys.stdout.write("\r" + "Processing: {0}/{1}".format(it, total))
+        datafile[key]['aur_version'] = str(parse_json(datafile[key]['aur_name']))
+        it += 1
+        sys.stdout.flush()
+    print()
+    return datafile
+
+
+def match_versions(datafile):
+    # Check whether 'version' and 'aur_version' is same or not.
+    for key in datafile.keys():
+        if datafile[key]['version'] != datafile[key]['aur_version']:
+            pprint(datafile[key])  # print particular info under this key.
+        else:
+            pass
+    return datafile
+
+
+def do_everything():
+    datafile: dict = open_main_json_file()
+    update_main_json_file(match_versions(update_from_api(update_local(datafile))))
 
 
 def main():
-    aurScrapper = AurScrapper()
-    try:
-        while True:
-            print(
-                "Choose your option:"
-                "\n\t1. Update info from AUR for Db Packages"
-                "\n\t2. Update info of Local Repo to Db"
-                "\n\t3. Check updates"
-                "\n\t4. Check all 1,2,3 one serially"
-                "\n\t5. Clear screen"
-                "\n\t6. Exit"
-            )
-            ans: str = input('Execute task no: ').strip()
-            if ans == '1':
-                aurScrapper.aurDbUpdate()
-                continue
-            elif ans == '2':
-                aurScrapper.localDbUpdate()
-                continue
-            elif ans == '3':
-                aurScrapper.check()
-                break
-            elif ans == '4':
-                aurScrapper.aurDbUpdate()
-                aurScrapper.localDbUpdate()
-                aurScrapper.check()
-                break
-            elif ans == '5':
-                subprocess.run('clear')
-                continue
-            elif ans == '6':
-                aurScrapper.close()
-                break
-            else:
-                print('Wrong input')
-                continue
-        aurScrapper.close()
-        sys.exit('Exited')
-    except KeyboardInterrupt:
-        aurScrapper.close()
+    print("Your options:")
+    print("\t1. Update Local Version")
+    print("\t2. Update Version From AUR")
+    print("\t3. Compare Version")
+    print("\t4. Update All")
+    print("\t5. Clear Screen")
+    print("\t6. Exit")
+    c: int = int(input("Enter Your Choice: "))
+
+    if c is 1:
         print()
-        sys.exit(-1)
-    except Exception as e:
-        aurScrapper.close()
-        sys.exit(e)
+        datafile: dict = open_main_json_file()
+        update_main_json_file(update_local(datafile))
+        print("File Updated Locally\n")
+        main()
+    elif c is 2:
+        print()
+        datafile: dict = open_main_json_file()
+        update_main_json_file(update_from_api(datafile))
+        print("File Updated From API\n")
+        main()
+    elif c is 3:
+        print()
+        datafile: dict = open_main_json_file()
+        update_main_json_file(match_versions(datafile))
+        main()
+    elif c is 4:
+        print()
+        do_everything()
+        print("Everything Updated!\n")
+        main()
+    elif c is 5:
+        os.system('clear')
+        print("\nCleared\n")
+        main()
+    elif c is 6:
+        print("\nExited")
+        sys.exit(0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
